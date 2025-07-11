5e100ac16701f1a6ef8225d8e5cabeff36715217
yulealmeida <yule.alme2@gmail.com>
2025-07-11 11:57:36 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,11 +18,19 @@ public class Program {
         Seller seller = sellerDao.findById(5);
         System.out.println(seller);
 
-        System.out.println("=== TEST 2: Seller findByDepatment ===");
+        System.out.println("\n=== TEST 2: Seller findByDepatment ===");
         Department department = new Department(2,null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 3: Seller findAll===");
+        list = sellerDao.findAll();
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
+
+
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -84,11 +84,51 @@ public class SellerDaoJDBC implements SellerDao {
         dep.setId(rs.getInt("DepartmentId"));
         dep.setName(rs.getString("DepName"));
         return dep;
+
+
     }
 
     @Override
     public List<Seller> findAll () {
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "ORDER BY Name ");
+
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()){
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if(dep == null){
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"),dep);
+                }
+
+
+                Seller obj = istantiateSaller(rs,dep);
+                list.add(obj);
+
+
+            }
+            return list;
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
+
     }
 
     @Override
